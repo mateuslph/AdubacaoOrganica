5bc7637373ecb4ab1ce37d4a330c1ea848916c97
mateuslph <mlptche@gmail.com>
2022-05-06 19:14:00 -0300

    corrigido erro ponto flutuante

--- Units/backup/umain.pas
+++ Units/backup/umain.pas
@@ -27,14 +27,13 @@ type
     procedure cbTipoDejetoChange(Sender: TObject);
     procedure cbTipoNutrChange(Sender: TObject);
     procedure edtQrnKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
-    procedure edtQrnKeyPress(Sender: TObject; var Key: char);
     procedure materiaSeca();
     procedure consentracaoNutriente();
     procedure calculo();
   private
-         var tipoNutr, dejeto : integer;
+         var tipoNutr, dejeto, v1, v2 : integer;
          var saida2, qrn, ms, c, ie, n, p, k, mn, mp, mk : real;
-         var qrns, saida2s : string;
+         var qrns, saida2s, stpt, stre, st1, st2, st3, stv : string;
   public
 
   end;
@@ -53,15 +52,6 @@ begin
   tipoNutr := cbTipoNutr.ItemIndex;
 end;
 
-procedure TDejetos.edtQrnKeyDown(Sender: TObject; var Key: Word;
-  Shift: TShiftState);
-begin
-     If (Key = 13) Then
-     begin
-          calculo();
-     end;
-end;
-
 procedure TDejetos.cbTipoDejetoChange(Sender: TObject);
 begin
   dejeto := cbTipoDejeto.ItemIndex;
@@ -70,6 +60,100 @@ end;
 procedure TDejetos.btnOkClick(Sender: TObject);
 begin
      calculo();
+end;
+
+procedure TDejetos.edtQrnKeyDown(Sender: TObject; var Key: Word;
+  Shift: TShiftState);
+begin
+     If (Key = 13) Then
+     begin
+          calculo();
+     end;
+
+    stpt := edtQrn.Text;
+    v1 := length(stpt);
+    if (v1 <> 0) then
+    begin
+      v2 := v1 - 1;
+    end;
+    if (v2 <> 0) then
+    begin
+      Delete(stpt, 1, v2);
+    end;
+    if (stpt = '.') then
+    begin
+      stpt := ',';
+    end;
+    stre := edtQrn.Text;
+    v1 := length(stre);
+    if (v1 <> 0) then
+    begin
+      Delete(stre, v1, 1);
+      st1 := stre + stpt;
+      st2 := stre + stpt;
+      st3 := stre + stpt;
+      stv := stre + stpt;
+      Delete(st1, 2, 2);
+      Delete(st2, 1, 1);
+      Delete(st2, 2, 1);
+      Delete(st3, 1, 2);
+    end;
+    if (st1 = ',') and (st2 <> ',') and (st3 <> ',') then
+    begin
+      ShowMessage('Não iniciar com separador decimal');
+      Delete(stv, v1, 1);
+      edtQrn.Text := '';
+      edtQrn.Text := (stv);
+      edtQrn.SelStart := v1;
+      exit;
+    end;
+    if (st1 <> ',') and (st2 = ',') and (st3 = ',') then
+    begin
+      ShowMessage('Digite apenas um separador decimal');
+      Delete(stv, v1, 1);
+      edtQrn.Text := '';
+      edtQrn.Text := (stv);
+      edtQrn.SelStart := v1;
+      exit;
+    end;
+    if (st1 = ',') and (st2 <> ',') and (st3 = ',') then
+    begin
+      ShowMessage('Digite apenas um separador decimal');
+      Delete(stv, v1, 1);
+      edtQrn.Text := '';
+      edtQrn.Text := (stv);
+      edtQrn.SelStart := v1;
+      exit;
+    end;
+    if (st1 = ',') and (st2 = ',') and (st3 <> ',') then
+    begin
+      ShowMessage('Digite apenas um separador decimal');
+      Delete(stv, v1, 1);
+      edtQrn.Text := '';
+      edtQrn.Text := (stv);
+      edtQrn.SelStart := v1;
+      exit;
+    end;
+    if (st1 <> ',') and (st2 <> ',') and (st3 = ',') then
+    begin
+      ShowMessage('Não terminar com separador decimal');
+      Delete(stv, v1, 1);
+      edtQrn.Text := '';
+      edtQrn.Text := (stv);
+      edtQrn.SelStart := v1;
+      exit;
+    end;
+    if (v1 <> 0) then
+    begin
+      edtQrn.Text := '';
+      edtQrn.Text := (stv);
+      edtQrn.SelStart := v1;
+    end
+    else
+    begin
+      edtQrn.Text := '';
+    end;
+
 end;
 
 procedure TDejetos.materiaSeca();
